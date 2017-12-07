e9d5bd9d747307a30f0ecf12131c631effcedbd5
atlit06 <atlit06@ru.is>
2017-12-07 17:50:35 +0000

    fixed failing tests

--- server/socket-app/tictactoe/tictactoe-state.js
+++ server/socket-app/tictactoe/tictactoe-state.js
@@ -2,17 +2,24 @@ const _ = require('lodash');
 
 module.exports = function (injected) {
 
+    var gamefull = false;
+
     return function (history) {
         function processEvent(event) {
+          if (event.type === "GameJoined") {
+            gamefull = true;
+          }
         }
-
         function processEvents(history) {
             _.each(history, processEvent);
         }
-
+        function gameFull() {
+          return gamefull;
+        }
         processEvents(history);
 
         return {
+            gameFull:gameFull,
             processEvents: processEvents,
         }
     };
